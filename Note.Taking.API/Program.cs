17b3fc1994719c5c664b2cb503a765c07ad1dac2
kharalambos-buranis <kharalambos.buranis@gmail.com>
2025-06-19 22:30:37 +0400

    Feature: Add authorization button in Swagger and enable cursor support in OpenAPI

--- Note.Taking.API/Program.cs
+++ Note.Taking.API/Program.cs
@@ -1,27 +1,59 @@
-
 using FluentValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Note.Taking.API.Common.Extensions;
-using Note.Taking.API.Features.Auth;
 using Note.Taking.API.Infrastructure.Database;
 using Note.Taking.API.Infrastructure.Services;
-using System.Text;
+using Scalar.AspNetCore;
 using Serilog;
-
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Host.UseSerilog((ctx, config) =>
+    config.ReadFrom.Configuration(ctx.Configuration));
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => {
+    options.CustomSchemaIds(t => t.FullName?.Replace('+', '.'));
+
+    options.AddSecurityDefinition("Bearer",
+           new OpenApiSecurityScheme
+           {
+               In = ParameterLocation.Header,
+               Type = SecuritySchemeType.Http,
+               Name = "Authorization",
+               Description = "Please enter access token",
+               BearerFormat = "JWT",
+               Scheme = "bearer",
+           });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+               new OpenApiSecurityScheme
+               {
+                   Reference = new OpenApiReference
+                   {
+                       Type=ReferenceType.SecurityScheme,
+                       Id="Bearer"
+                   }
+               },
+               Array.Empty<string>()
+            }
+        });
+});
 
 builder.Services.AddDbContext<AppDbContext>(
       optionsBuilder =>
       {
           optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-      }
-      );
+      });
 
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
 
@@ -32,67 +64,38 @@ builder.Services.AddAuthorization();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(o =>
     {
-        o.TokenValidationParameters = new TokenValidationParameters 
+        o.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuerSigningKey = true,
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!)),
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
-            
+
         };
     });
 
-builder.Services.AddEndpoints();
-
-
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
-builder.Services.AddValidatorsFromAssemblyContaining<RefreshToken>();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Host.UseSerilog((ctx, config) =>
-    config.ReadFrom.Configuration(ctx.Configuration));
+builder.Services.AddEndpoints();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
+
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.MapScalarApiReference();
 }
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
-
 app.UseAuthentication();
-
-
-
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//};
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//});
+app.UseAuthorization();
 
 app.MapEndpoints();
 
 app.Run();
-
-//internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-//{
-//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-//}
